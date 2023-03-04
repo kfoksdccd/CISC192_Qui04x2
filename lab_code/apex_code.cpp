f40d5fa77b32047aa7b6b88e4bfcd8a1d4b29f66
kfoksdccd <126300842+kfoksdccd@users.noreply.github.com>
2023-03-04 14:35:09 -0800

    Quiz#04

--- lab_code/apex_code.cpp
+++ lab_code/apex_code.cpp
@@ -7,6 +7,29 @@ using namespace std;
 
 void quiz(stringstream& cin, stringstream& cout)
 {
-    
+    //Prompt user to enter number of rows
+    //Output as follows:  Note about the space vs. , below
+    //Test#1
+    //1	
+    //1,2,3,4,5,6,7,8,9 1,2,3,4,5,6,7,8,9 1,2,3,4,5,6,7,8,9
+    //
+    //Test#2
+    //2	
+    //1,2,3,4,5,6,7,8,9 1,2,3,4,5,6,7,8,9 1,2,3,4,5,6,7,8,9
+    //2,4,6,8,10,12,14,16,18 2,4,6,8,10,12,14,16,18 2,4,6,8,10,12,14,16,18
+    //
+    //Test#3
+    //4	
+    //1,2,3,4,5,6,7,8,9 1,2,3,4,5,6,7,8,9 1,2,3,4,5,6,7,8,9
+    //2,4,6,8,10,12,14,16,18 2,4,6,8,10,12,14,16,18 2,4,6,8,10,12,14,16,18
+    //3,6,9,12,15,18,21,24,27 3,6,9,12,15,18,21,24,27 3,6,9,12,15,18,21,24,27
+    //4,8,12,16,20,24,28,32,36 4,8,12,16,20,24,28,32,36 4,8,12,16,20,24,28,32,36
+
+    //Submit your apex_code.cpp to Canvas
+    //Submit the repl.it URL in the comments inside apex_code.cpp
+    //Hint:
+    //  -Start with one repeat-pattern at a time, 1,2,3,4,5,6,7,8,9
+    //  -Then, look into 1,2,3,4,5,6,7,8,9 1,2,3,4,5,6,7,8,9 1,2,3,4,5,6,7,8,9
+    //Hint: Deal with space vs. , the last.
 }
 
